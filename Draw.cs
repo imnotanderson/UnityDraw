f0805f2106283c9ffee07d2cefc75ed439531a07
imnotanderson <imnotanderson@users.noreply.github.com>
2017-09-15 10:30:48 +0800

    Add files via upload

---
+++ Draw.cs
@@ -0,0 +1,51 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+    public class Draw : MonoBehaviour
+    {
+        public Camera cam;
+
+        void Start()
+        {
+            cam = GetComponent<Camera>();
+        }
+
+        void Update()
+        {
+            if (!Input.GetMouseButton(0))
+                return;
+
+            RaycastHit hit;
+            if (!Physics.Raycast(cam.ScreenPointToRay(Input.mousePosition), out hit))
+                return;
+
+            Renderer rend = hit.transform.GetComponent<Renderer>();
+            MeshCollider meshCollider = hit.collider as MeshCollider;
+            if (rend == null || rend.sharedMaterial == null || rend.sharedMaterial.mainTexture == null ||
+                meshCollider == null)
+                return;
+            Texture2D tex = rend.material.mainTexture as Texture2D;
+            Vector2 pixelUV = hit.textureCoord;
+            pixelUV.x *= tex.width;
+            pixelUV.y *= tex.height;
+            tex.SetPixel((int) pixelUV.x, (int) pixelUV.y, Color.black);
+            tex.Apply();
+        }
+ 
+        [ContextMenu("New")]
+        public void NewPlane()
+        {
+            var obj = GameObject.CreatePrimitive(PrimitiveType.Plane);
+            obj.transform.rotation = Quaternion.Euler(new Vector3(-90, 0, 0));
+            var render = obj.GetComponent<MeshRenderer>();
+            render.materials = new Material[]
+            {
+                new Material(Shader.Find("Draw"))
+            };
+            var t = new Texture2D(20, 20);
+            t.wrapMode = TextureWrapMode.Clamp;
+            t.anisoLevel = 0;
+            t.filterMode = FilterMode.Point;
+            render.sharedMaterial.mainTexture = t;
+        }
+    }
---
+++ draw.shader
@@ -0,0 +1,46 @@
+﻿Shader "Draw"
+{
+    Properties{
+        _MainTex("texture", 2D) = "white"{}
+    }
+
+    SubShader{
+        LOD 100
+
+        Pass{
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+            #include "UnityCG.cginc"
+
+            sampler2D _MainTex;
+            fixed4 _MainTex_ST;
+
+            struct vIn{
+                half4 vertex:POSITION;
+                float2 texcoord:TEXCOORD0;
+                fixed4 color:COLOR;
+            };
+
+            struct vOut{
+                half4 pos:SV_POSITION;
+                float2 uv:TEXCOORD0;
+                fixed4 color:COLOR;
+            };
+
+            vOut vert(vIn v){
+                vOut o;
+                o.pos = mul(UNITY_MATRIX_MVP, v.vertex);
+                o.uv = TRANSFORM_TEX(v.texcoord, _MainTex);
+                o.color = v.color;
+                return o;
+            }
+
+            fixed4 frag(vOut i):COLOR{
+                fixed4 tex = tex2D(_MainTex, i.uv);
+                return tex * i.color;
+            }
+            ENDCG
+        }
+    }
+}
